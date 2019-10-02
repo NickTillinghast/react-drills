788cfdf2a50053c780c032d38f3dd81221da42a7
Nick Tillinghast <nickt2@cox.net>
2019-10-01 17:11:24 -0700

    second commit

--- app-4/src/index.js
+++ app-4/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -6,42 +6,3 @@ import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
-
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: "",
-      password: ""
-    };
-    this.handleLogin = this.handleLogin.bind(this);
-  }
-
-  handleUsernameChange(name) {
-    this.setState({ username: name });
-  }
-
-  handlePasswordChange(pass) {
-    this.setState({ password: pass });
-  }
-
-  handleLogin() {
-    alert(`Username: ${this.state.username} Password: ${this.state.password}`);
-  }
-  render() {
-    return (
-      <div>
-        <input
-          onChange={e => this.handleUsernameChange(e.target.value)}
-          type="text"
-        />
-        <input
-          onChange={e => this.handlePasswordChange(e.target.value)}
-          type="text"
-        />
-        <button onClick={this.handleLogin}>Login</button>
-      </div>
-    );
-  }
-}
-export default Login;
